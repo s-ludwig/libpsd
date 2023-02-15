ba15915dbaef80d7b8f78aac52c2b3a4d62229a3
Sönke Ludwig <sludwig@outerproduct.org>
2023-02-15 20:01:24 +0100

    Add script for CI based building.

---
+++ build.sh
@@ -0,0 +1,17 @@
+if [ $OS == "linux" ]; then
+	make -f Makefile
+elif [ $OS == "macos" ]; then
+	CFLAGS=--target=$TARGET make -f Makefile
+elif [ $OS == "ios" ]; then
+	if [[ $TARGET == *"-simulator" ]]; then
+		make -f Makefile.iphoneSimulator
+	else
+		make -f Makefile.iphone
+	fi
+elif [ $OS == "windows" ]; then
+	cd psd
+	MSBuild.exe psd.sln /property:Configuration=Release /property:Platform=$WINDOWS_PLATFORM
+else
+	echo Unknown OS: $OS
+	exit 1
+fi
